1c2ae0a536c47c9d95fa20048f6b2dc97685cee3
Mike Scott <mike.scott@sqs.com>
2018-12-29 03:17:14 +0000

    Start tool install script

---
+++ README.md
@@ -0,0 +1,5 @@
+iOS Scripts
+===========
+
+
+Some scripts to help with iOS setup and dev on Macs
---
+++ install_tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew tap caskroom/cask
+
+brew install bash-git-prompt
+#brew cask install visual-studio-code
+
+
+
+brew cleanup       
