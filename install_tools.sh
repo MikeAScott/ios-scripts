08f8e0889887c92c429fad693b77de7207b10ae2
Mike Scott <mike.scott@nttdata.com>
2020-01-09 10:37:22 +0000

    Add Ruby, Ansible, AWS CLI, and Azure CLI

--- install_tools.sh
+++ install_tools.sh
@@ -3,21 +3,39 @@
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
-brew tap caskroom/cask
+brew tap homebrew/cask-cask
 
 brew install git
 brew install bash-git-prompt
 
 brew cask install visual-studio-code
 
-# Ruby dev
+# Dev tools
 brew install autoconf automake gdbm gmp libksba libtool libyaml openssl pkg-config readline gpg2
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# Ruby dev
+brew install rbenv
+rbenv init
+echo 'eval "$(rbenv init - )"' >> ~/.bash_profile
 
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+rbenv install 2.6.5
+
+# Python3 Pip3 & Virtualenv
+brew install python
+pip3 install virtualenv
+
+#Install Ansible
+pip3 install ansible paramiko boto3 botocore molecule
 
 # DotNet Dev
 brew cask install dotnet-sdk
 
+# Terraform
+brew install terraform
+
+# AWS & Azure CLI
+pip3 install awscli
+brew install azure-cli
+
+# Tidy up
 brew cleanup       
